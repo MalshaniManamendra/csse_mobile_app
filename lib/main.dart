48c9392c4215cfff0823641efce0efe3b6c94b5d
MalshaniManamendra <malshanimanamendra@gmail.com>
2019-09-24 10:49:52 +0530

    Create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:csse_mobile_app/Home.dart';
 
 void main()=>runApp(
   new MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Train Service',
+    home: Home(),
   )
 );
